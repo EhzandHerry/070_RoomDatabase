bedb896ee51e667ce3abb887c029e3d3d3b1fd0b
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-04 20:47:37 +0700

    memberi komentar pada file AplikasiSiswa

--- app/src/main/java/com/example/activity7database/AplikasiSiswa.kt
+++ app/src/main/java/com/example/activity7database/AplikasiSiswa.kt
@@ -5,6 +5,9 @@ import com.example.activity7database.Repository.ContainerApp
 import com.example.activity7database.Repository.ContainerDataApp
 
 class AplikasiSiswa : Application() {
+    /**
+     * AappContainer instance digunakan oleh kelas-kelas lainnya untuk mendapatkan dependensi
+     */
     lateinit var container: ContainerApp
 
     override fun onCreate(){
