25801922a4884c5d2ee0b25f5788d1fbc2800447
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-04 19:41:44 +0700

    membuat class HomeViewModel

---
+++ app/src/main/java/com/example/activity7database/model/HomeViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.activity7database.model
+
+import androidx.lifecycle.ViewModel
+import com.example.activity7database.Repository.RepositorySiswa
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
+
+}
