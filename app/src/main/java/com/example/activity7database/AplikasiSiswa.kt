5a7689c450bdef637f6e0dac19c5b4eeb4093d36
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-04 20:06:31 +0700

    membuat ContainerDataApp untuk AplikasiSiswa

--- app/src/main/java/com/example/activity7database/AplikasiSiswa.kt
+++ app/src/main/java/com/example/activity7database/AplikasiSiswa.kt
@@ -1,6 +1,14 @@
 package com.example.activity7database
 
 import android.app.Application
+import com.example.activity7database.Repository.ContainerApp
+import com.example.activity7database.Repository.ContainerDataApp
 
 class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate(){
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
 }
