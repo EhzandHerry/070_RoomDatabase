ad4c9b4d79d18ba941b4af2f66426b1c6f494e7a
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-04 19:52:31 +0700

    membuat data class HomeUiState pada HomeViewModel

--- app/src/main/java/com/example/activity7database/model/HomeViewModel.kt
+++ app/src/main/java/com/example/activity7database/model/HomeViewModel.kt
@@ -2,10 +2,17 @@ package com.example.activity7database.model
 
 import androidx.lifecycle.ViewModel
 import com.example.activity7database.Repository.RepositorySiswa
+import com.example.activity7database.data.Siswa
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class HomeViewModel(private val repositorySiswa: RepositorySiswa): ViewModel(){
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
 }
