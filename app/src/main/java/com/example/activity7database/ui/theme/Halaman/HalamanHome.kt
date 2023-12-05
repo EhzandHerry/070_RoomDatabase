efa8f668842d9869ef47b3d33ed676cca7b6bbef
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-05 19:33:11 +0700

    membuat fun ListSiswa pada HalamanHome

--- app/src/main/java/com/example/activity7database/ui/theme/Halaman/HalamanHome.kt
+++ app/src/main/java/com/example/activity7database/ui/theme/Halaman/HalamanHome.kt
@@ -1,8 +1,10 @@
 package com.example.activity7database.ui.theme.Halaman
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -93,6 +95,23 @@ fun BodyHome(
             ListSiswa(
                 itemSiswa = itemSiswa,
                 modifier = Modifier.padding(horizontal = dimensionResource(id = R.dimen.padding_small))
+            )
+        }
+    }
+}
+
+@Composable
+fun ListSiswa(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemSiswa, key = { it.id }) {
+                person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
             )
         }
     }
