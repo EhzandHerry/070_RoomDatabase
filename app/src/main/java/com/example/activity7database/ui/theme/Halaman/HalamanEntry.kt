f49b430af83a3d31df76d008b8109c3578fe235a
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-05 18:59:22 +0700

    membuat object DestinasiEntry pada HalamanEntry.kt

---
+++ app/src/main/java/com/example/activity7database/ui/theme/Halaman/HalamanEntry.kt
@@ -0,0 +1,9 @@
+package com.example.activity7database.ui.theme.Halaman
+
+import com.example.activity7database.R
+import com.example.activity7database.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
