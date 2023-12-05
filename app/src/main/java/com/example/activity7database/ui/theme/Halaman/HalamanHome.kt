810520ddb3828214cc46e41168518220928999b4
EhzandHerry <ehzand.herry1926@gmail.com>
2023-12-05 19:20:27 +0700

    membuat object DestinasiHome pada HalamanHome

---
+++ app/src/main/java/com/example/activity7database/ui/theme/Halaman/HalamanHome.kt
@@ -0,0 +1,9 @@
+package com.example.activity7database.ui.theme.Halaman
+
+import com.example.activity7database.R
+import com.example.activity7database.navigasi.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
